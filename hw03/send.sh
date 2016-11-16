9c4c997ed7b2f6df10ebf6d38a65be77d54967b9
Michal Horacek <horacmi1@gmail.com>
2016-11-16 17:24:40 +0100

    web traffic generator

--- hw03/send.sh
+++ hw03/send.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
 
+# main object size         (lognorm) : m = 8.346, s^2 = 1.866
+# inline object size       (lognorm) : m = 6.166, s^2 = 5.581
+# # of cached requests     (geom)    : p = 0.37
+# # of non cached requests (lognorm) : n = 1.848, s^2 = 1.371
+# parsing time             (gamma)   : a = 0.483, b = 0.269
+# # of inline objects      (gamma)   : a = 0.237, b = 23.416
+# inter-arrival time       (gamma)   : a = 0.16,  b = 5.375
+# viewing time             (weibull) : a = 0.484, b = 18.534
+
+
+TARGET_IP="10.10.1.3.4133"
+
+
 random_gamma() {
     out=$(rg -I $next_init -D Gamma -a $1 -b $2)
     gamma_value=$(echo $out | awk -F' :' '{print $1}')
@@ -29,8 +42,15 @@ random_weibull() {
 }
 
 
+# Float -> Int
 round() {
     LC_ALL=C printf "%.0f" $1
+}
+
+
+# random 4B unsigned int
+random_uint() {
+    head -c 4 /dev/random | od -N4 -tu4 | head -1 | awk -F' ' '{print $2}'
 }
 
 
@@ -40,33 +60,18 @@ generate_inline_object_count() {
 }
 
 
+# $1: data size
 send_object() {
     echo "Sending $1 bytes of data"
-}
-
-
-generate_traffic() {
-    # generate initial seed
-    next_init=$(head -c 4 /dev/urandom | od -N4 -tu4 | \
-                head -1 | awk -F' ' '{print $2}')
-
-    # generate number of non-cached and cached objects
-    random_lognormal 1.848 1.371
-    NON_CACHED_OBJECTS=$(round $lognormal_value)
-
-    random_geometric 0.37 # 0.44
-    CACHED_OBJECTS=$(round $geometric_value)
-
-    # send all non-cached objects
-    echo "Sending $NON_CACHED_OBJECTS non-cached objects"
-    for (( i = 0; i < $NON_CACHED_OBJECTS; i++ )); do
-        send_noncached_request
-    done
-
-    # send all cached objects
-    echo "Sending $CACHED_OBJECTS cached objects"
-    for (( i = 0; i < $CACHED_OBJECTS; i++ )); do
-        send_cached_request
+    tg_parameters=`cat <<-EOF
+	on 3
+	tcp $TARGET_IP
+	setup
+	arrival exponential 0.02 length exponential 576
+	seed 0 data $1
+	EOF`
+    while (! echo "$tg_parameters" | tg -f > /dev/null ); do
+        echo "Restarting"
     done
 }
 
@@ -104,9 +109,11 @@ send_noncached_request() {
     # send all inline objects
     echo "Sending $inline_object_count inline objects"
     for (( i = 0; i < inline_object_count; i++ )); do
+        # send inline object
         random_lognormal 6.166 5.581
         send_object $lognormal_value
 
+        # sleep inter-arrival time
         random_gamma 0.16 5.375
         echo "Inter-arrival time: $gamma_value"
         microsleep $gamma_value
@@ -116,6 +123,31 @@ send_noncached_request() {
     random_weibull 0.484 18.534
     echo "Viewing time: $weibull_value"
     microsleep $weibull_value
+}
+
+
+generate_traffic() {
+    # generate initial seed
+    init_seed=$(echo random_uint)
+    next_init=$(rg -S $init_seed | awk -F' :' '{print $2}')
+
+    # generate number of non-cached and cached requests
+    random_lognormal 1.848 1.371
+    NON_CACHED_OBJECTS=$(round $lognormal_value)
+    random_geometric 0.37 # 0.44
+    CACHED_OBJECTS=$(round $geometric_value)
+
+    # send all non-cached objects
+    echo "Sending $NON_CACHED_OBJECTS non-cached objects"
+    for (( i = 0; i < $NON_CACHED_OBJECTS; i++ )); do
+        send_noncached_request
+    done
+
+    # send all cached objects
+    echo "Sending $CACHED_OBJECTS cached objects"
+    for (( i = 0; i < $CACHED_OBJECTS; i++ )); do
+        send_cached_request
+    done
 }
 
 
